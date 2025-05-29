ef687e8d34a186c4028ba478010688ce85a57e4f
GitHub Action <action@github.com>
2025-05-29 08:26:42 +0000

    Template cleanup

--- src/main/kotlin/com/github/xfqy0617/visualizer4callhierarchy/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/xfqy0617/visualizer4callhierarchy/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.xfqy0617.visualizer4callhierarchy.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
--- src/main/kotlin/com/github/xfqy0617/visualizer4callhierarchy/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/xfqy0617/visualizer4callhierarchy/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.xfqy0617.visualizer4callhierarchy.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.xfqy0617.visualizer4callhierarchy.MyBundle
+import com.github.xfqy0617.visualizer4callhierarchy.services.MyProjectService
 import javax.swing.JButton
 
 
